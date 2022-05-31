ff1ef60be4706c09c7c7d43cac43001688424bec
hem_b <hemanthb93@gmail.com>
2022-05-31 08:34:46 +0530

    [Hemanth]:displays the list in reverse order

--- LinkedList/LinkedList/LinkedList.cs
+++ LinkedList/LinkedList/LinkedList.cs
@@ -23,10 +23,39 @@ namespace LinkedList
                 {
                     temp = temp.next;
                 }
-                temp = node;
+                temp.next = node;
             }
             Console.WriteLine("Data {0} is added to the list", data);
 
+        }
+
+        public void Displaylist()
+        {
+            Node temp = this.head;
+            if(temp == null)
+            {
+                Console.WriteLine("Linked List is empty!!");
+            }
+            while(temp != null)
+            {
+                Console.Write(temp.data+" -> ");
+                temp = temp.next;
+            }
+            Console.WriteLine();
+        }
+        public void ReverseList()
+        {
+            Node prev = null,current = head,next = null;
+            while(current != null)
+            {
+                next = current.next;
+                current.next = prev;
+                prev = current;
+                current = next;
+                
+            }
+            head = prev;
+            
         }
         
     }
--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -1,6 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 LinkedList.LinkedList linkedList = new LinkedList.LinkedList();
-linkedList.addNode(56);
-linkedList.addNode(30);
 linkedList.addNode(70);
+linkedList.addNode(30);
+linkedList.addNode(56);
+linkedList.Displaylist();
+linkedList.ReverseList();
+Console.WriteLine("Reversed list is -");
+linkedList.Displaylist();
 
