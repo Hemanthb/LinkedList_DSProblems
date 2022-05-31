906d37b60cdadecca2fde63e024f278717f15a46
hem_b <hemanthb93@gmail.com>
2022-05-31 08:43:30 +0530

    [Hemanth]:Displays th list by appending data

--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -1,10 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 LinkedList.LinkedList linkedList = new LinkedList.LinkedList();
-linkedList.addNode(70);
-linkedList.addNode(30);
 linkedList.addNode(56);
+linkedList.addNode(30);
+linkedList.addNode(70);
 linkedList.Displaylist();
-linkedList.ReverseList();
-Console.WriteLine("Reversed list is -");
-linkedList.Displaylist();
+
 
