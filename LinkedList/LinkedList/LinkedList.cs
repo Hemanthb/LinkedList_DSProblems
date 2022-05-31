852e7d1e67d8c0b9cd379b0cda45d53a28cec6dc
hem_b <hemanthb93@gmail.com>
2022-05-31 06:53:18 +0530

    [Hemnth]:Data is added to newly created linked list

---
+++ LinkedList/LinkedList/LinkedList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class LinkedList
+    {
+        Node head;
+        public void addNode(int data)
+        {
+            Node node = new Node(data);
+            if(head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp = node;
+            }
+            Console.WriteLine("Data {0} is added to the list", data);
+
+        }
+        
+    }
+}
---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+LinkedList.LinkedList linkedList = new LinkedList.LinkedList();
+linkedList.addNode(56);
+linkedList.addNode(30);
+linkedList.addNode(70);
+
